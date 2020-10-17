1e51494e88e77205dc7f7ea169c196a7c2c7f12d
shichaoshuai <2307755715@qq.com>
2020-10-17 17:18:33 +0800

    问题的恢复

--- routes/issue.js
+++ routes/issue.js
@@ -14,7 +14,7 @@ function getTopicCount(dbo) {
     return topicCount;
 }
 
-
+//查询全部正常的问题
 router.get("/issueAll", function (req, res) {
     
     // 获取得到页码和每页显示的条目数
@@ -47,7 +47,40 @@ router.get("/issueAll", function (req, res) {
     })
 
 })
+//查询问题回收站
+router.get("/issueDelAll", function (req, res) {
+    
+    // 获取得到页码和每页显示的条目数
+   
+    var pageNum = Number(req.query.pageNum);
+    var pageSize = Number(req.query.pageSize);
+    var skipValue = (pageNum - 1) * pageSize;
 
+    common.getMongoClient().then(async function (client) {
+        var dbo = client.db("newcapecForum");
+        // 查询总条目数
+        var topicCount = await getTopicCount(dbo);
+      
+
+        dbo.collection("questionModule").find({
+            $or:[{questionStatus:2},{questionStatus:"2"}] 
+        }).skip(skipValue).limit(pageSize).toArray(function (err, result) {
+            // 处理帖子id
+            for (var i = 0; i < result.length; i++) {
+                result[i]._id = result[i]._id.toString();
+            }
+         
+            // 结合populars.art渲染数据
+            res.render('issuedel.art', {
+                topiclist: result,
+                pageNum: pageNum,
+                pageCount: topicCount % pageSize == 0 ? topicCount / pageSize : parseInt(topicCount / pageSize) + 1
+            });
+        })
+    })
+
+})
+//删除问题
 router.get("/delteIssueById/:topicId", function (req, res) {
     var topicId = req.params.topicId;
      // 每访问一次,就记录一次
@@ -95,5 +128,51 @@ router.get("/delteIssueById/:topicId", function (req, res) {
     })
 
 })
+//恢复问题
+router.get("/recIssueById/:topicId", function (req, res) {
+    var topicId = req.params.topicId;
+     // 每访问一次,就记录一次
+    common.getMongoClient().then((client) => {
+        var dbo = client.db("newcapecForum"); // dbo就是指定的数据库对象
 
+        dbo.collection("questionModule").updateOne({
+            _id: ObjectId(topicId)
+        }, {
+            $set: {
+                questionStatus: 0
+            }
+        }, function (err, dbRes) {
+            if (err) throw err;
+            client.close();
+        })
+    })
+
+    // 获取得到页码和每页显示的条目数
+  
+    var pageNum = Number(req.query.pageNum);
+    var pageSize = Number(req.query.pageSize);
+    var skipValue = (pageNum - 1) * pageSize;
+    common.getMongoClient().then(async function (client) {
+        var dbo = client.db("newcapecForum");
+        // 查询总条目数
+        var topicCount = await getTopicCount(dbo);
+     
+
+        dbo.collection("questionModule").find({
+            $or:[{questionStatus:2},{questionStatus:"2"}] 
+        }).skip(skipValue).limit(pageSize).toArray(function (err, result) {
+            // 处理帖子id
+            for (var i = 0; i < result.length; i++) {
+                result[i]._id = result[i]._id.toString();
+            }
+            // 结合populars.art渲染数据
+            res.render('issuedel.art', {
+                topiclist: result,
+                pageNum: result.length == 0 ? pageNum - 1 : pageNum,
+                pageCount: topicCount % pageSize == 0 ? topicCount / pageSize : parseInt(topicCount / pageSize) + 1
+            });
+        })
+    })
+
+})
 module.exports = router
